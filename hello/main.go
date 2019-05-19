4d06a12e33b85317fc1c5ca0d2863bef691abf71
AnkerLeng <553628212@qq.com>
2019-05-20 01:26:44 +0800

    4-2 登陆简单逻辑介绍及实现

--- hello/main.go
+++ hello/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io"
 	"net/http"
 )
 
@@ -11,7 +10,34 @@ func main() {
 		//数据库操作
 		//逻辑处理
 		//rest api json/xml 返回
-		io.WriteString(writer, "hello world")
+
+		// 1. 获取前端传递的参数
+		// mobile, password
+		// 解析参数
+		// 如何获得参数
+		// 解析参数
+
+		_ = request.ParseForm()
+
+		mobile := request.PostForm.Get("mobile")
+		password := request.PostForm.Get("password")
+		loginok := false
+		if (mobile == "168000000000" && password == "123456") {
+			loginok = true
+		}
+		//使用application/x-www-form-urlencoded POST数据
+		//curl http://localhost:8080/user/login -X POST -d "mobile=186000000000&password=123456"
+		str := `{"code":0,"data":{"id":1,"token":"test"}}`
+		if (!loginok) {
+			//返回失败的json
+			str = `{"code":-1,"msg":"密码不正确"}`
+		}
+		//设置header为json 默认的text/html 所以特别指出返回的格式为
+		//为application/json
+		writer.Header().Set("Content-Type", "application/json")
+		//返回json ok
+		_, _ = writer.Write([]byte(str))
+
 	})
 
 	// 启动web服务器
