2a74554f8726795d19e8eb71c93cee48ab8a2aa7
AnkerLeng <553628212@qq.com>
2019-06-04 23:53:17 +0800

    6-2 分布式功能实现

--- hello/ctrl/contact.go
+++ hello/ctrl/contact.go
@@ -4,6 +4,7 @@ import (
 	"../args"
 	"../service"
 	"../util"
+	"GoChat/hello/model"
 	"net/http"
 )
 
@@ -27,14 +28,26 @@ func LoadCommunity(w http.ResponseWriter, req *http.Request) {
 }
 func JoinCommunity(w http.ResponseWriter, req *http.Request) {
 	var arg args.ContactArg
-
 	//如果这个用的上,那么可以直接
 	util.Bind(req, &arg)
 	err := contactService.JoinCommunity(arg.Userid, arg.Dstid);
+	//todo 刷新用户的群组信息
+	AddGroupId(arg.Userid, arg.Dstid)
 	if err != nil {
 		util.RespFail(w, err.Error())
 	} else {
 		util.RespOk(w, nil, "")
+	}
+}
+func CreateCommunity(w http.ResponseWriter, req *http.Request) {
+	var arg model.Community
+	//如果这个用的上,那么可以直接
+	util.Bind(req, &arg)
+	com, err := contactService.CreateCommunity(arg);
+	if err != nil {
+		util.RespFail(w, err.Error())
+	} else {
+		util.RespOk(w, com, "")
 	}
 }
 
